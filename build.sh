163d7a92dbc11962f2b7f0c35b9b571e58fbd4e9
Guanyu Wu <lambda@funcptr.org>
2020-09-24 18:55:41 -0400

    Fixed semicolons, as well as broke up i3-status generation into parts.

--- build.sh
+++ build.sh
@@ -7,16 +7,25 @@ mkdir -p build
 
 for file in i3/*.config; do
     echo "# From: $file" >> build/config.i3
-    cat $file >> build/config.i3;
+    cat $file >> build/config.i3
     echo "" >> build/config.i3
 done
 
 if [ -f "i3/host-specific/$(hostname -s).config" ]; then
     echo "# From: i3/host-specific/$(hostname -s).config" >> build/config.i3
     cat "i3/host-specific/$(hostname -s).config" >> build/config.i3
-fi;
+fi
 
-cat "i3/i3status/theme" >> build/config.i3status;
+for file in i3status/*.config; do
+    echo "# From: $file" >> build/config.i3status
+    cat $file >> build/config.i3status
+    echo "" >> build/config.i3status
+done
+
+if [ -f "i3status/host-specific/$(hostname -s).config" ]; then
+    echo "# From: i3status/host-specific/$(hostname -s).config" >> build/config.i3status
+    cat "i3status/host-specific/$(hostname -s).config" >> build/config.i3status
+fi
 
 cp Xresources/.Xresources build/.Xresources
 
@@ -30,6 +39,6 @@ fi
 
 for file in zsh/*.zshrc; do
     echo "# From: $file" >> build/.zshrc
-    cat $file >> build/.zshrc;
+    cat $file >> build/.zshrc
     echo "" >> build/.zshrc
 done
